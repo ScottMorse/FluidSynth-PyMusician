0fde43dda0a3f2e12ef2c0e36e2393d1b30945b9
Scott Morse <scottmorsedev@gmail.com>
2019-02-23 15:25:48 -0600

    Implementing threading for multi-part music

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import numpy as np
 import pyaudio
 import fluidsynth
 import pymusician as pm
+from threading import Thread
 
 SAMPLE_RATE = 44100
 TEMPO = 120
 SOUNDFONT_FILE = "niceKeys23.sf2"
+PARTS = 3
 
 pa = pyaudio.PyAudio()
 strm = pa.open(
@@ -17,7 +19,21 @@ strm = pa.open(
 
 fl = fluidsynth.Synth()
 sfid = fl.sfload(SOUNDFONT_FILE)
-fl.program_select(0, sfid, 0, 25)
+
+for ch in range(PARTS):
+    fl.program_select(ch, sfid, 0, 25)
+
+class TurnOffLater(Thread):
+
+    def __init__(self,midi_num,wait_time):
+        super().__init__()
+        self.midi_num = midi_num
+        self.wait_time = wait_time
+    
+    def run(self):
+        time.sleep(self.wait_time)
+        fl.noteoff(0,self.midi_num)
+
 
 # takes PyMusician Note objects as args
 # The Notes must have octave and rhythm
@@ -25,29 +41,44 @@ fl.program_select(0, sfid, 0, 25)
 def play_notes(*notes,velocity=100,last=False):
     samples = []
 
-    rhythm_value = notes[0].rhythm.value
     notes_temp = []
-    
+
+    notes = list(notes)
+    notes.sort(key=lambda n: n.rhythm.value)
+
     for note in notes:
         midi_num = note.hard_pitch + 12
         fl.noteon(0,midi_num,velocity)
-        notes_temp.append(midi_num)
+        notes_temp.append((midi_num,note.rhythm.value))
 
-    samples = np.append(samples, fl.get_samples(round(SAMPLE_RATE * (60 / TEMPO) * (rhythm_value / 128))))
+    frames = round(SAMPLE_RATE * (60 / TEMPO) * (notes[0].rhythm.value / 128))
+
+    samples = np.append(samples,fl.get_samples(frames))
     
-    for midi_val in notes_temp:
-        fl.noteoff(0,midi_val)
-    
+    previous = None
+    for midi_val,rhythm_val in notes_temp:
+        if previous:
+            if previous < rhythm_val:
+                rhythm_len = (60 / TEMPO) * (note.rhythm.value / 128) 
+                proportion_to_prev = (rhythm_len - ((60 / TEMPO) * (previous / 128))) / rhythm_len
+                TurnOffLater(midi_val,(60 / TEMPO) * (note.rhythm.value / 128) * proportion_to_prev).start()
+            else:
+                fl.noteoff(0,midi_val)
+        else:
+            fl.noteoff(0,midi_val)
+        previous = rhythm_val
+
     samples = np.append(samples,fl.get_samples(round(SAMPLE_RATE * (0.02 if not last else 2))))
-    
+
     strm.write(fluidsynth.raw_audio_string(samples))
 
 if __name__ == "__main__":
 
-    play_notes(pm.Note('C',4,'3'),pm.Note('E',4,'3'))
+    play_notes(pm.Note('C',4,'2'),pm.Note('E',4,'3'))
     play_notes(pm.Note('D',4,'3'),pm.Note('F',4,'3'))
     play_notes(pm.Note('E',4,'3'),pm.Note('G',4,'3'))
-    play_notes(pm.Note('G',4,'3'),pm.Note('C',5,'3'),last=True)
+    play_notes(pm.Note('F',4,'3'),pm.Note('B',4,'3'))
+    play_notes(pm.Note('E',4,'3'),pm.Note('C',5,'3'),last=True)
 
 fl.delete()
 strm.close()
